b48363ee8ce55d43f4cd67acac5091a10499671c
Gun-0927 <105433082+Gun-0927@users.noreply.github.com>
2024-07-23 18:09:32 +0900

    commit
    commit

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    public int nextMove;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        Invoke("Think",5);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Move
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+        //PlatformCheck
+        Vector2 forntVec = new Vector2(rigid.position.x+ nextMove * 0.5f, rigid.position.y);
+        Debug.DrawRay(forntVec, Vector3.down, new Color(0, 1, 0));
+
+        RaycastHit2D rayHit = Physics2D.Raycast(forntVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+        if (rayHit.collider == null)
+        {
+            Turn();
+        }
+    }
+
+    //Àç±ÍÇÔ¼ö
+    void Think()
+    {
+        //Set Next Active
+        nextMove = Random.Range(-1, 2);
+
+        //Sprite Animation
+        animator.SetInteger("WalkSpeed", nextMove);
+        //Filp Sprite
+        if(nextMove != 0)
+        {
+            spriteRenderer.flipX = nextMove == 1;
+        }
+        //Àç±Í
+        float nextThinkTime = Random.Range(2f,6f);
+        Invoke("Think", nextThinkTime);
+    }
+
+    void Turn()
+    {
+        nextMove *= -1;
+        spriteRenderer.flipX = nextMove == 1;
+
+        CancelInvoke();
+        Invoke("Think", 5);
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -85,4 +85,36 @@ public class PlayerMove : MonoBehaviour
         }
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            OnDamaged(collision.transform.position);
+        }
+    }
+    void OnDamaged(Vector2 targetPos)
+    {
+        //Change Layer
+        gameObject.layer = 10;
+
+        //View Alpha
+        spriteRenderer.color = new Color(1, 1, 1,0.4f);
+
+        //Reaction Force
+        int dirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
+        rigid.AddForce(new Vector2(dirc, 1)*7 , ForceMode2D.Impulse);
+
+
+        //Animation
+        animator.SetTrigger("doDamaged");
+        Invoke("OffDamaged", 3);
+        
+    }
+
+    void OffDamaged()
+    {
+        gameObject.layer = 9;
+        spriteRenderer.color = new Color(1, 1, 1, 1);
+    }
 }
